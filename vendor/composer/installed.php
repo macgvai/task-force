a12849520190aeaf3b79fd2d8300d84652917a0e
Viktor <st.viktorr@gmail.com>
2025-01-26 16:12:27 +0300

    1.20

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'taskforce/taskforce',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b73403adf890e672907a0ac5db516359c88fc90f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'taskforce/taskforce' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b73403adf890e672907a0ac5db516359c88fc90f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
